bc7491bb22eda4d2af5330293d8d282453e99d0e
ted <ted@yahoo.com>
2016-10-25 19:36:50 +0800

    hi

--- app/src/main/java/com/ted/ted102501/MainActivity.java
+++ app/src/main/java/com/ted/ted102501/MainActivity.java
@@ -8,13 +8,23 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btn2;
+    Button btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn2=(Button)findViewById(R.id.button2);
-        MyListener oo=new MyListener();
-        btn2.setOnClickListener(oo);
+        //MyListener oo=new MyListener();
+        //btn2.setOnClickListener(oo);
+        btn2.setOnClickListener(new MyListener());
+
+        btn3=(Button)findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                Log.d("方式3:","button click...");
+            }
+        });
     }
 
     class MyListener implements View.OnClickListener  //member inner class
@@ -24,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("方式2:","button click..");
         }
     }
+
     public void click1(View v){
 
         Log.d("方式1:","button click.");
